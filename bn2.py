d19ac716500e87753506ec4c2b77d58fc697a44b
Ayyoub125 <youyoujty@gmail.com>
2025-02-04 08:41:09 +0100

    bn2.py

--- bn2.py
+++ bn2.py
@@ -27,10 +27,7 @@ def peut_placer_bateau(grille, x, y, taille_bateau, horizontal):
     x (int)
     y (int)
     taille_bateau (int)
-    horizontal (bool)
-    
-    Retourne :
-    bool: True si le bateau peut être placé, False sinon.
+    horizontal.
     """
     for i in range(taille_bateau):
         if horizontal:
@@ -41,74 +38,12 @@ def peut_placer_bateau(grille, x, y, taille_bateau, horizontal):
                 return False
     return True
 
-def placer_bateau(grille, taille_bateau):
-    """
-    Place un bateau du premier joueur avec le numero 1
-    
-    Paramètres :
-    grille (list)
-    taille_bateau (int)
-    """
-    horizontal = random.choice([True, False])  # Le bateau peut être placé horizontalement ou verticalement
-
-    if horizontal:
-        x = random.randint(0, len(grille) - 1)  # Ligne aléatoire
-        y = random.randint(0, len(grille[0]) - taille_bateau)  # Colonne de départ aléatoire
-
-        while not peut_placer_bateau(grille, x, y, taille_bateau, horizontal):  # Vérifie si c'est possible
-            x = random.randint(0, len(grille) - 1)
-            y = random.randint(0, len(grille[0]) - taille_bateau)
-
-        for i in range(taille_bateau):  # Place le bateau sur la grille
-            grille[x][y + i] = 1
-    else:
-        x = random.randint(0, len(grille) - taille_bateau)
-        y = random.randint(0, len(grille[0]) - 1)
-
-        while not peut_placer_bateau(grille, x, y, taille_bateau, horizontal):
-            x = random.randint(0, len(grille) - taille_bateau)
-            y = random.randint(0, len(grille[0]) - 1)
-
-        for i in range(taille_bateau):
-            grille[x + i][y] = 1
-
-def placer_bateau2(grille, taille_bateau):
-    """
-    Place un bateau du deuxième joueur avec le numero 2
-    
-    Paramètres :
-    grille (list)
-    taille_bateau (int)
-    """
-    horizontal = random.choice([True, False])
-
-    if horizontal:
-        x = random.randint(0, len(grille) - 1)
-        y = random.randint(0, len(grille[0]) - taille_bateau)
-
-        while not peut_placer_bateau(grille, x, y, taille_bateau, horizontal):
-            x = random.randint(0, len(grille) - 1)
-            y = random.randint(0, len(grille[0]) - taille_bateau)
-
-        for i in range(taille_bateau):
-            grille[x][y + i] = 2
-    else:
-        x = random.randint(0, len(grille) - taille_bateau)
-        y = random.randint(0, len(grille[0]) - 1)
-
-        while not peut_placer_bateau(grille, x, y, taille_bateau, horizontal):
-            x = random.randint(0, len(grille) - taille_bateau)
-            y = random.randint(0, len(grille[0]) - 1)
-
-        for i in range(taille_bateau):
-            grille[x + i][y] = 2
-
 def afficher_grille(grille):
     """
-    Affiche la grille.
+    Affiche la grille
     
     Paramètres :
-    grille (list)
+    grille
     """
     for ligne in grille:
         print(ligne)
@@ -127,11 +62,11 @@ def jouer():
         if grille[x][y] == 0:
             grille[x][y] = 'X'  # Met un 'X' si tir raté
             afficher_grille(grille)
-            print("Coup dans l'eau !")
+            print("Coup dans l'eau")
         elif grille[x][y] == 1 or grille[x][y] == 2:
             grille[x][y] = 'O'  # Met un 'O' si tir touché
             afficher_grille(grille)
-            print("Touché !")
+            print("Touché")
         else:
             print("Cet endroit a déjà été tiré.")
 
@@ -142,11 +77,11 @@ def jouer():
         if grille[x][y] == 0:
             grille[x][y] = 'X'  # Met un 'X' si tir raté
             afficher_grille(grille)
-            print("Coup dans l'eau !")
+            print("Coup dans l'eau")
         elif grille[x][y] == 1 or grille[x][y] == 2:
             grille[x][y] = 'O'  # Met un 'O' si tir touché
             afficher_grille(grille)
-            print("Touché !")
+            print("Touché")
         else:
             print("Cet endroit a déjà été tiré.")
         
